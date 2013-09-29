b0884a6bfdec74fb84c40ed64482a41c15dfa923
O. R. Lissenberg <olissenberg@gmail.com>
2013-09-29 10:29:08 +0200

    Renaming & CSS classes.

--- lp-champion.php
+++ lp-champion.php
@@ -1,7 +1,7 @@
 <?php
 
-$b = $build->build;
-$c = $build->build->champion;
+$lolBuild = $build->build;
+$lolChampion = $build->build->champion;
 
 global $libChampion;
 global $libRune;
@@ -9,28 +9,30 @@ global $libMastery;
 
 ?>
 
-<table>
+<table class="lp-champion">
     <tr>
         <td>
-            <div style="float:left" data-champion-code="<?php echo $c->code; ?>" class="champions-icon champions-<?php echo $c->code; ?>"></div>
+            <div style="float:left" data-champion-code="<?php echo $lolChampion->code; ?>"
+                 class="champions-icon champions-<?php echo $lolChampion->code; ?>"></div>
         </td>
         <td>
-            <h3><?php echo $libChampion[$c->code]["longName"]; ?></h3>
+            <h3><?php echo $libChampion[$lolChampion->code]["longName"]; ?></h3>
         </td>
     </tr>
 </table>
 
 <h3>Abilities:</h3>
-<table>
+
+<table class="lp-abilities">
     <?php
 
-    foreach ($c->abilities as $ability) {
+    foreach ($lolChampion->abilities as $ability) {
         echo '<tr>';
         echo '<td><div style="float:left" data-champion-code="' . $ability->code . '" class="champions-icon champions-' .
             \LeaguePress\WordPressPlugin::toCssCode(
-                $c->code . '-' . $ability->code
+                $lolChampion->code . '-' . $ability->code
             ) . '"></div></td>';
-        echo "<td>" . $libChampion[$c->code]["abilities"][$ability->code]["shortName"] . "</td>";
+        echo "<td>" . $libChampion[$lolChampion->code]["abilities"][$ability->code]["shortName"] . "</td>";
         for ($i = 1; $i < 19; $i++) {
             $show = array_search($i, $ability->levels) !== false ? $i : "";
             echo "<td>$show</td>";
@@ -40,8 +42,10 @@ global $libMastery;
 
     ?>
 </table>
+
 <h3>Summoner Spells:</h3>
-<table>
+
+<table class="lp-spells">
     <tr>
         <td>
             <?php
@@ -56,9 +60,10 @@ global $libMastery;
         </td>
     </tr>
 </table>
+
 <h3>Runes:</h3>
 
-<table>
+<table class="lp-runes">
     <?php
 
     foreach ($build->build->runes as $type) {
@@ -77,12 +82,11 @@ global $libMastery;
 
 <h3>Items:</h3>
 
-
 <?php
 
 foreach ($build->build->items as $itemsets) {
     echo "<h5>{$itemsets->title}</h5>";
-    echo '<ul style="display:table; width:600px;">';
+    echo '<ul class="lp-items" style="display:table; width:600px; list-style-type:none;">';
     foreach ($itemsets->items as $item) {
         echo '<li style="float: left;"><div data-item-code="' . $item->code . '" data-item-index="'
             . $index . '" class="items-icon items-' . \LeaguePress\WordPressPlugin::toCssCode($item->code) . '"></div></li>';
@@ -96,7 +100,7 @@ foreach ($build->build->items as $itemsets) {
 
 <?php
 
-echo '<ul style="display:table; width:600px; list-style-type:none; padding:0; margin:0;">';
+echo '<ul class="lp-masteries" style="display:table; width:600px; list-style-type:none; padding:0; margin:0;">';
 foreach ($build->build->masteries as $mastery) {
     echo '<li style="float:left; width:300px; height:48px;">' .
         '<div style="position: relative; display: inline-block; float: left;" data-mastery-code="' . $mastery->code . '" title="' .
@@ -110,3 +114,4 @@ echo '</ul>';
 
 ?>
 <br/><br/><br/>
+<!-- ;p -->
